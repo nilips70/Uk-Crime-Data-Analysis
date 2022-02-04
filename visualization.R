1508eecc19b57f997236cd499d305e4ff57d0d0f
Niloufar Pourshir Sefidi <46159878+nilips70@users.noreply.github.com>
2022-02-04 20:19:44 +0000

    + time series

--- visualization.R
+++ visualization.R
@@ -9,14 +9,14 @@ library(leaflet)
 library(leaflet.extras)
 library(RColorBrewer)
 library(lubridate)
+library(plotly)
 rm(list=ls())
 
 #reading the dataset
-london_ss <- readRDS("london_ss.rds")
-london_st <- readRDS("london_st.rds")
-london_income <- readRDS("london_income.rds")
-df_st <- readRDS("df_st.rds")
-df_ss <- readRDS("df_ss.rds")
+london_ss <- readRDS("df_ss.rds")
+london_st <- readRDS("df_st_london.rds")
+london_others <- readRDS("london_others.rds")
+df_st <- readRDS("df_st_london.rds")
 
 #################     LONDON ST     #################     
 #################     data exploring / preparation    #################
@@ -79,9 +79,13 @@ leaflet() %>%
 
 
 # different crime types and locations
-pal1 <- colorBin("viridis", london_income$`Median Annual Household Income estimate`, 8, pretty = FALSE)
+pal1 <- colorBin("viridis", london_others$median_annual_income_household, 8, pretty = FALSE)
+pal2 <- colorBin("viridis", london_others$`Unemployment Rate`, 8, pretty = FALSE)
 
-london_income <- london_income %>% mutate(pop1 = paste0(`Median Annual Household Income estimate`, " at ", LSOA11NM))
+#pal2 <- colorFactor(palette = c("#FF6600", "#FF9900","#FFCC66" ), levels = c(">21" , "14.5 - 21", "14.5>"))
+
+london_others <- london_others %>% mutate(pop1 = paste0(round(median_annual_income_household), " at ", LSOA11NM),
+                                          pop2 = paste0(`Unemployment Rate`, " at ", LSOA11NM))
 
 
 leaflet() %>%
@@ -90,6 +94,7 @@ leaflet() %>%
   addHeatmap(data = weapon, lng = ~Longitude, lat = ~Latitude, intensity = 10 , max = 100, radius=20, blur = 10, group = "Possession of weapons") %>%
   addHeatmap(data = bike_theft, lng = ~Longitude, lat = ~Latitude, intensity = 10 , max = 100, radius=20, blur = 10, group = "Bicycle theft") %>%
   addHeatmap(data = other_theft, lng = ~Longitude, lat = ~Latitude, intensity = 10 , max = 100, radius=20, blur = 10, group = "Other theft") %>% 
+  addHeatmap(data = burglary, lng = ~Longitude, lat = ~Latitude, intensity = 10 , max = 100, radius=20, blur = 10, group = "Burglary") %>% 
   addHeatmap(data = drugs, lng = ~Longitude, lat = ~Latitude, intensity = 10 , max = 100, radius=20, blur = 10, group = "Drugs") %>%
   addHeatmap(data = public_order, lng = ~Longitude, lat = ~Latitude, intensity = 10 , max = 100, radius=20, blur = 10, group = "Public order") %>%
   addHeatmap(data = shoplifting, lng = ~Longitude, lat = ~Latitude, intensity = 10 , max = 100, radius=20, blur = 10, group = "Shoplifting") %>%
@@ -98,7 +103,7 @@ leaflet() %>%
   addHeatmap(data = robbery, lng = ~Longitude, lat = ~Latitude, intensity = 10 , max = 100, radius=20, blur = 10, group = "Robbery") %>%
   addHeatmap(data = theft_person, lng = ~Longitude, lat = ~Latitude, intensity = 10 , max = 100, radius=20, blur = 10, group = "Theft from the person") %>%
   addHeatmap(data = other_crime, lng = ~Longitude, lat = ~Latitude, intensity = 10 , max = 100, radius=20, blur = 10, group = "Other crime") %>%
-  addPolygons(data = london_income, fillColor = ~pal1(london_income$`Median Annual Household Income estimate`),
+  addPolygons(data = london_others, fillColor = ~pal1(london_others$median_annual_income_household),
               fillOpacity = 0.7,
               group = "Income",
               weight = 0.2,
@@ -108,20 +113,35 @@ leaflet() %>%
                 color = "#666",
                 fillOpacity = 0.2,
                 bringToFront = TRUE),
-              label=london_income$pop1,
+              label=london_others$pop1,
               labelOptions = labelOptions(
                 style = list("font-weight" = "normal", padding = "3px 8px"),
                 textsize = "15px",
                 direction = "auto"))  %>%
+  addPolygons(data = london_others, fillColor = ~pal2(london_others$`Unemployment Rate`),
+              fillOpacity = 0.7,
+              group = "Unemployment Rate",
+              weight = 0.2,
+              smoothFactor = 0.2,
+              highlight = highlightOptions(
+                weight = 5,
+                color = "#666",
+                fillOpacity = 0.2,
+                bringToFront = TRUE),
+              label=london_others$pop2,
+              labelOptions = labelOptions(
+                style = list("font-weight" = "normal", padding = "3px 8px"),
+                textsize = "15px",
+                direction = "auto")) %>% 
   addLayersControl(
     overlayGroups = c( "Criminal damage and arson", "Possession of weapons", "Bicycle theft", "Other theft","Burglary", "Drugs","Public order", 
                        "Shoplifting", "Vehicle crime", "Violence and sexual offences",
-                       "Robbery", "Theft from the person", "Other crime", "Income"),
+                       "Robbery", "Theft from the person", "Other crime", "Income", "Unemployment Rate"),
     options = layersControlOptions(collapsed = T),
     position = "bottomleft"
   ) %>% hideGroup(c("Possession of weapons", "Bicycle theft", "Other theft","Burglary", "Drugs","Public order", 
                     "Shoplifting", "Vehicle crime", "Violence and sexual offences",
-                    "Robbery", "Theft from the person", "Other crime", "Income"))
+                    "Robbery", "Theft from the person", "Other crime", "Income", "Unemployment Rate"))
 
 
 
@@ -141,10 +161,10 @@ true_outcome <- london_ss %>% filter(outcome_linked_to_object_of_search == TRUE)
 
 table(true_outcome$Outcome)
 #???????????????????????????????????????
-df_eth_true_out <- true_outcome %>% mutate(self_defined_ethnicity = as.logical.factor(self_defined_ethnicity)) %>% 
+#df_eth_true_out <- true_outcome %>% mutate(self_defined_ethnicity = as.logical.factor(self_defined_ethnicity)) %>% 
   filter(self_defined_ethnicity)
 
-df_eth_true_out <- true_outcome %>% mutate(officer_defined_ethnicity = as.factor(officer_defined_ethnicity)) %>% 
+#df_eth_true_out <- true_outcome %>% mutate(officer_defined_ethnicity = as.factor(officer_defined_ethnicity)) %>% 
   filter(officer_defined_ethnicity)
 
 # age vs gender
@@ -154,10 +174,67 @@ ggplot(london_ss, aes( x = reorder(age, age, function(x)-length(x)), fill = Gend
 
 # hour vs age
 ggplot(london_ss, aes(x = hour(Date), y = age)) + geom_boxplot()
-ggplot(london_ss, aes(x = hour(Date), y = age)) + geom_violin()
 
 # hour vs gender
 ggplot(london_ss, aes(x = hour(Date), y = Gender)) + geom_boxplot()
-ggplot(london_ss, aes(x = hour(Date), y = Gender)) + geom_violin()
-ggplot(london_ss, aes(x = hour(Date), fill = Gender)) + geom_density(alpha = .5)
 
+##############################################################################
+##############              Time Series               ########################
+##############################################################################
+#aggregating crime numbers based on a month
+crime_agg <- london_st %>% select(crime_type, Month) %>% group_by(Month) %>% summarise(value = n()) %>% 
+  mutate(Month = ym(Month))
+
+#time series crimes for 3 years
+ggplot(crime_agg, aes(x = Month, y = value)) + geom_line() + theme_minimal()
+
+#aggregating different crime types based on a month 
+crime_by_type_all <- london_st %>% select(crime_type, Month) %>% group_by(Month, crime_type) %>% summarise(value = n()) %>% 
+  mutate(Month = ym(Month))
+
+#time series for each crime in 3 years
+c <- ggplot(crime_by_type_all, aes(x = Month, y = value, color = crime_type)) + geom_line() + theme_minimal()
+ggplotly(c)
+
+#aggregating crimes based on a month between 2018 and 2020 (before COVID-19 hit)
+seasonality_total_2019 <- london_st %>% mutate(Month = ym(Month)) %>% 
+  filter(Month < ym("2020-07")) %>%
+  select(crime_type, Month) %>% group_by(Month) %>% summarise(value = n()) 
+
+#time series crimes until COVID-19 hit   
+c1 <- ggplot(seasonality_total_2019, aes(x = Month, y = value)) + geom_line() + theme_minimal()
+ggplotly(c1)
+
+#aggregating each crime type based on a month between 18 and 2020 (before COVID-19 hit)
+seasonality_by_type_2019 <- london_st %>% mutate(Month = ym(Month)) %>% 
+  filter(Month < ym("2020-04")) %>%
+  select(crime_type, Month) %>% group_by(Month, crime_type) %>% summarise(value = n()) 
+
+#time series different crime types before COVID 19 hit
+c2 <- ggplot(seasonality_by_type_2019, aes(x = Month, y = value, color = crime_type)) + geom_line()
+ggplotly(c2)
+
+#correlation of different crime types on monthly basis
+corr = seasonality_by_type_2019_long = seasonality_by_type_2019 %>% 
+  pivot_wider(names_from = crime_type,  values_from = value) %>% 
+  ungroup() %>% 
+  select(-Month) %>% cor()
+
+corrplot::corrplot(corr, type = "upper", title = "2019")
+
+
+
+#################     LONDON SS     #################     
+#################     data exploring / preparation    #################
+#aggregating outcome 
+hourly <- london_ss %>% filter(Date < ym("2020-04")) %>% mutate(time = hour(Date)) %>% 
+  group_by(Outcome, time) %>% summarise(value = n()) %>% group_by(time) %>% 
+  mutate(total_stop = sum(value),
+         rate = round(value/total_stop, 2)) %>% 
+  filter(total_stop > 50, Outcome != "A no further action disposal") %>% 
+  na.omit()
+
+#
+
+c3 <- ggplot(hourly, aes(x = time, y = rate, color = Outcome)) + geom_line()
+ggplotly(c3)
